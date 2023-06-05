d074974968fcb461dc88ad19a34271817f37538f
Novri Parande <128975661+NovriParande@users.noreply.github.com>
2023-06-05 16:10:12 +0800

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+
+$servername = "localhost";
+$database = "db_novriparande";
+$username = "root";
+$password = "";
+
+$koneksi = mysqli_connect($servername, $username, $password, $database);
+?>
